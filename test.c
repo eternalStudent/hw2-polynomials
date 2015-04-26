ed0b78dbbc5182300a1d785ee0c32aababc2a099
Dean Ayalon <ayalondean@gmail.com>
2015-04-26 12:02:26 +0300

    Revert "test"
    This reverts commit 1076ab482d9ac73546d26af68acf3349dd81f96d.

--- test.c
+++ test.c
@@ -2,10 +2,27 @@
 #include "polynomial.c"
 
 int main(){
-	struct polynomial* p;
-	p = polynomial_new("p");
-	polynomial_addCoefficient(p, 5.0f, 3);
-	polynomial_print(p);
-	polynomial_free(p);
+	struct polynomial* p1;
+	p1 = polynomial_new("p1");
+	float c0 = 1;
+	float c3 = 0.4f;
+	float c4 = 1.3f;
+	polynomial_addCoefficient(p1, &c0, 0);
+	polynomial_addCoefficient(p1, &c3, 3);
+	polynomial_addCoefficient(p1, &c4, 4);
+	polynomial_print(p1);
+	
+	struct polynomial* p2;
+	p2 = polynomial_new("p2");
+	float d2 = 5.4f;
+	float d9 = 1.0f;
+	polynomial_addCoefficient(p2, &d2, 2);
+	polynomial_addCoefficient(p2, &d9, 9);
+	polynomial_print(p2);
+	
+	struct polynomial* sum;
+	sum = polynomial_sum(p1, p2);
+	polynomial_print(sum);
+	
 	return 0;
 }
