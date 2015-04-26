1076ab482d9ac73546d26af68acf3349dd81f96d
Dean Ayalon <ayalondean@gmail.com>
2015-04-26 11:09:35 +0300

    test

--- polynomial.h
+++ polynomial.h
@@ -9,7 +9,7 @@ struct polynomial* polynomial_new(char* name);
 
 void nothing(void* v);
 
-void polynomial_addCoefficient(struct polynomial *p, float* coefficient, int pow);
+void polynomial_addCoefficient(struct polynomial *p, float coefficient, int pow);
 
 float polynomial_getCoefficient(struct polynomial *p, int i);
 
@@ -18,6 +18,4 @@ int polynomial_rank(struct polynomial *p);
 void polynomial_print(struct polynomial *p);
 
 void polynomial_free(struct polynomial *p);
-
-struct polynomial* polynomial_sum(struct polynomial* p1, struct polynomial* p2);
 
