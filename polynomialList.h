c985457bfc853b287221581b55fbcea15f4c25a2
eternalStudent <noam.weisberg@gmail.com>
2015-04-27 14:10:06 +0300

    polynomialList

---
+++ polynomialList.h
@@ -0,0 +1,11 @@
+#include "arrayList.h"
+#include "polynomial.h"
+
+
+struct arrayList* polynomialList_new();
+
+int polynomialList_add(struct arrayList* polynomials, struct polynomial* p);
+
+struct polynomial* polynomialList_getByName(struct arrayList* polynomials, char* name);
+
+void polynomialList_free(struct arrayList* polynomials);
